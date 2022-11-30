91649d7d9d1d236077b043aa99ef6cbd0234348e
michelle <michellekjs@kaist.ac.kr>
2022-11-30 16:10:02 +0900

    working on weather api

--- laundry_200/src/component/WeatherPage.js
+++ laundry_200/src/component/WeatherPage.js
@@ -2,6 +2,10 @@ import Navigation from "./NavigationBar.js";
 import Weather from "./Weather.js";
 import useWindowDimensions from "./WindowSize.js";
 import { Grid, Toolbar, Typography } from "@mui/material";
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+
 
 const circle = (e) => {
   return {
@@ -46,11 +50,35 @@ const styles = {
 
 const WeatherPage = () => {
   const { height, width } = useWindowDimensions();
+  const [ arr ,setarr ] = useState([])
+
+
+  const weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=36&lon=127&appid=3a3acc40ffe3a923536f1a5f132edcd6"
+
+  
+  const jsonArray = new Array(40)
+
+  async function getWeather() {
+    await axios.get(weatherUrl).then((response) => {
+      for (var i=0 ; i<response.data.list.length; i++) {
+        jsonArray[i] = ((response.data.list[i]))
+      }        
+      console.log(jsonArray)
+    })
+  }
+
+  window.onload = function() {
+    getWeather()
+    setarr(jsonArray)
+  }
+
+  console.log(arr)
+
+
   return (
     <>
       <Navigation />
       <Toolbar />
-
       <div style={styles.horizontal}>
         <Grid container justifyContent="space-between">
           <Grid item>
@@ -79,8 +107,7 @@ const WeatherPage = () => {
                 marginRight: "50px",
               }}
             >
-              {" "}
-              27 degrees{" "}
+            {arr[0].main.temp}degrees
             </div>{" "}
             Celcius{" "}
           </div>
@@ -115,16 +142,23 @@ const WeatherPage = () => {
         </div>
 
         {/* weather forecast for the week */}
-        <div style={{ marginTop: "60px" }}> Weather Forecast for the week </div>
+        <div style={{ marginTop: "60px" }}> Weather Forecast for the week  - humidity</div>
         <div style={styles.horizontal}>
-          <Weather day="monday" weather="sunny" />
-          <Weather day="monday" weather="sunny" />
-          <Weather day="monday" weather="sunny" />
-          <Weather day="monday" weather="sunny" />
-          <Weather day="monday" weather="sunny" />
-          <Weather day="monday" weather="sunny" />
-          <Weather day="monday" weather="sunny" />
+          <Weather day={arr[8].dt_txt} weather="sunny" />
+          <Weather day={arr[16].dt_txt} weather="sunny" />
+          <Weather day={arr[24].dt_txt} weather="sunny" />
+          <Weather day={arr[32].dt_txt} weather="sunny" />
+
         </div>
+
+        <div style={{ marginTop: "60px" }}> Weather Forecast for today  - by time</div>
+          <div style={styles.horizontal}>
+          <Weather day={arr[0].dt_txt} weather="sunny" />
+          <Weather day={arr[1].dt_txt} weather="sunny" />
+          <Weather day={arr[2].dt_txt} weather="sunny" />
+          <Weather day={arr[3].dt_txt} weather="sunny" />
+          <Weather day={arr[4].dt_txt} weather="sunny" />
+          </div>
       </div>
     </>
   );
