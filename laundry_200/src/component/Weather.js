86e8181f9b516845f312a19491c11223d48e1908
Siang Jo Yee <joyeesiang@gmail.com>
2022-12-09 14:28:29 +0900

    Fixed minor errors and added geolocation api

--- laundry_200/src/component/Weather.js
+++ laundry_200/src/component/Weather.js
@@ -1,20 +1,35 @@
 //get input from the weather
-import Rain from "../image/rainy.png"
-import Clear from "../image/sunny.png"
-import Snow from "../image/snowy.png"
-import Clouds from "../image/cloudy.png"
+import Rain from "../image/rainy.png";
+import Clear from "../image/sunny.png";
+import Snow from "../image/snowy.png";
+import Clouds from "../image/cloudy.png";
 
 function Weather(props) {
   return (
-    <div style={{	display: "flex",
-		justifyContent: "center",
-		alignItems: "center",
-		flexDirection: "column", margin:"20px"}}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        margin: "20px",
+      }}
+    >
       <div> {props.day} </div>
       <div> {props.weather} </div>
-      <img src= {props.weather} />
+      <img
+        src={
+          props.weather === "Rain"
+            ? Rain
+            : props.weather === "Clear"
+            ? Clear
+            : props.weather === "Snow"
+            ? Snow
+            : Clouds
+        }
+      ></img>
     </div>
-  ) 
+  );
 }
 
 export default Weather;
